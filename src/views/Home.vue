8494cef9b9c7f30008abfd03a6dbc80c03378c0d
WIN-5R1NLRV2N64\Administrator <yaweiwei3@gmail.com>
2021-10-19 12:41:17 -0400

    ts严格模式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,148 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <div class="leftArrow" @click="leftMove"><button> 《 </button></div>
+    <div class="container" :style="{left: state.imgLeft + 'px'}">
+      <div  class="image" v-for="(item, index) in state.list" :key="index">
+        <img :src="require('@/assets/' + item)" />
+      </div>
+    </div>
+    <div class="rightArrow" @click="rightMove()"><button> 》 </button></div>
+    <div class="dots">
+      <div class="dot" :class="{active: index === state.currentPage}" v-for="(item, index) in state.list" :key="index">
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { reactive } from 'vue'
+export default {
+  name: 'App',
+  setup () {
+    const state = reactive({
+      list: ['1.png', '2.png', '3.png'],
+      imgLeft: 0,
+      timeval: 1,
+      currentPage: 0
+    })
 
-@Options({
-  components: {
-    HelloWorld
+    function leftMove () {
+      let moveDist = 0
+      if (state.imgLeft <= -800 * (state.list.length - 1)) {
+        state.imgLeft = -800 * (state.list.length - 1)
+      } else {
+        setInterval(() => {
+          if (moveDist <= -800) {
+            clearInterval()
+          } else {
+            moveDist -= Math.round(8 / state.timeval)
+            state.imgLeft -= Math.round(8 / state.timeval)
+          }
+        }, 10)
+        state.currentPage++
+      }
+    }
+
+    function rightMove () {
+      let moveDist = 0
+      if (state.imgLeft >= 0) {
+        state.imgLeft = 0
+      } else {
+        setInterval(() => {
+          if (moveDist >= 800) {
+            clearInterval()
+          } else {
+            moveDist += Math.round(8 / state.timeval)
+            state.imgLeft += Math.round(8 / state.timeval)
+          }
+        }, 10)
+        state.currentPage--
+      }
+    }
+
+    return {
+      state,
+      leftMove,
+      rightMove
+    }
   }
-})
-export default class Home extends Vue {}
+}
 </script>
+
+<style>
+.app {
+  position: relative;
+  width: 800px;
+  height: 600px;
+  margin: 200px auto;
+  overflow: hidden;
+}
+
+button {
+  width: 50px;
+  height: 100px;
+  font-size: 30px;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  background-color: rgba(0,0,0,0.2);
+}
+
+button:hover {
+  background-color: rgba(0,0,0,0.8);
+}
+
+.leftArrow {
+  position: absolute;
+  top: 242px;
+  left: 0px;
+  z-index: 5;
+}
+
+.rightArrow {
+  position: absolute;
+  top: 242px;
+  left: 750px;
+  z-index: 5;
+}
+
+.container {
+  position: absolute;
+  top: 0;
+  left: 0px;
+  width: 2400px;
+  display: flex;
+  flex-wrap: nowrap;
+}
+
+.image {
+  width: 800px;
+  /* height: 600px; */
+  font-size: 100px;
+}
+
+img {
+  width: 800px;
+}
+
+.dots {
+  position: absolute;
+  bottom: 20px;
+  right: 50px;
+}
+
+.dot {
+  float: left;
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+  text-align: center;
+  border-radius: 50%;
+  background: #aaa;
+  margin-right: 10px;
+}
+
+.active {
+  background: red;
+}
+</style>
